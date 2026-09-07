6f77c6bbaa4f9d5bdf45162ecac294b6302a91c1
kplayerogaming-max <kplayerogaming@gmail.com>
2026-09-07 16:01:55 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,15 +1,12 @@
 // ==========================================
-// PHONG NHÂN THÁNH VỰC
+// PHONG NHAN THANH VUC
 // SUPABASE CONFIGURATION
 // ==========================================
 
-// Supabase Project URL
-const SUPABASE_URL = "https://ewydvnjgjwfotaawkepl.supabase.co"
+const SUPABASE_URL = "https://ewydvnjgjwfotaawkepl.supabase.co";
 
-// Supabase Publishable Key
-const SUPABASE_PUBLISHABLE_KEY =sb_publishable_H5HfvGakf7JrbQuiGiNsPQ_PCEt81d7
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_H5HfvGakf7JrbQuiGiNsPQ_PCEt81d7";
 
-// Tạo Supabase client
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_PUBLISHABLE_KEY
