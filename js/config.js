ddc657dc3ce3bee2a6a19f0ef69f6481f1fcdc16
kplayerogaming-max <kplayerogaming@gmail.com>
2026-09-09 22:19:21 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ const SUPABASE_URL = "https://ewydvnjgjwfotaawkepl.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_H5HfvGakf7JrbQuiGiNsPQ_PCEt81d7";
 
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_PUBLISHABLE_KEY
+window.supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
 );
